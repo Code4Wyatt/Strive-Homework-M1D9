0d531c83ccc13aab0e20ee056d80fa31a904dd83
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-07-01 19:14:31 +0100

    Applied attempt
    Attempt at applying styling to the div with the same value if it is the result

--- d9.js
+++ d9.js
@@ -2,9 +2,234 @@ const randomOne = function() {
         return Math.floor(Math.random() * 76);
         let randomResult = function() {
             if randomOne === 1 {
-               let applyClass = document.querySelector("container")[0];
+               let applyClass = document.getElementsByClassName("container")[0];
                applyClass.className = "selected";
 
+               } else if randomOne === 2 {
+                   let applyClass = document.getElementsByClassName("container")[1];
+                   applyClass.className = "selected";
+               } else if randomOne === 3 {
+                   let applyClass = document.getElementsByClassName("container")[2];
+                   applyClass.className = "selected";
+               } else if randomOne === 4 {
+                   let applyClass = document.getElementsByClassName("container")[3];
+                   applyClass.className = "selected";
+               } else if randomOne === 5 {
+                let applyClass = document.getElementsByClassName("container")[4];
+                applyClass.className = "selected";
+               } else if randomOne === 6 {
+                let applyClass = document.getElementsByClassName("container")[5];
+                applyClass.className = "selected";
+               } else if randomOne === 7 {
+                let applyClass = document.getElementsByClassName("container")[6];
+                applyClass.className = "selected";
+               } else if randomOne === 8 {
+                let applyClass = document.getElementsByClassName("container")[7];
+                applyClass.className = "selected";
+               } else if randomOne === 9 {
+                let applyClass = document.getElementsByClassName("container")[8];
+                applyClass.className = "selected";
+               } else if randomOne === 10 {
+                let applyClass = document.getElementsByClassName("container")[9];
+                applyClass.className = "selected";
+               } else if randomOne === 11 {
+                let applyClass = document.getElementsByClassName("container")[10];
+                applyClass.className = "selected";
+               } else if randomOne === 12 {
+                let applyClass = document.getElementsByClassName("container")[11];
+                applyClass.className = "selected";
+               } else if randomOne === 13 {
+                let applyClass = document.getElementsByClassName("container")[12];
+                applyClass.className = "selected";
+               } else if randomOne === 14 {
+                let applyClass = document.getElementsByClassName("container")[13];
+                applyClass.className = "selected";
+               } else if randomOne === 15 {
+                let applyClass = document.getElementsByClassName("container")[14];
+                applyClass.className = "selected";
+               } else if randomOne === 16 {
+                let applyClass = document.getElementsByClassName("container")[15];
+                applyClass.className = "selected";
+               } else if randomOne === 17 {
+                let applyClass = document.getElementsByClassName("container")[16];
+                applyClass.className = "selected";
+               } else if randomOne === 18 {
+                let applyClass = document.getElementsByClassName("container")[17];
+                applyClass.className = "selected";
+               } else if randomOne === 19 {
+                let applyClass = document.getElementsByClassName("container")[18];
+                applyClass.className = "selected";
+               } else if randomOne === 20 {
+                let applyClass = document.getElementsByClassName("container")[19];
+                applyClass.className = "selected";
+               } else if randomOne === 21 {
+                let applyClass = document.getElementsByClassName("container")[20];
+                applyClass.className = "selected";
+               } else if randomOne === 22 {
+                let applyClass = document.getElementsByClassName("container")[21];
+                applyClass.className = "selected";
+               } else if randomOne === 23 {
+                let applyClass = document.getElementsByClassName("container")[22];
+                applyClass.className = "selected";
+               } else if randomOne === 24 {
+                let applyClass = document.getElementsByClassName("container")[23];
+                applyClass.className = "selected";
+               } else if randomOne === 25 {
+                let applyClass = document.getElementsByClassName("container")[24];
+                applyClass.className = "selected";
+               } else if randomOne === 26 {
+                let applyClass = document.getElementsByClassName("container")[25];
+                applyClass.className = "selected";
+               } else if randomOne === 27 {
+                let applyClass = document.getElementsByClassName("container")[26];
+                applyClass.className = "selected";
+               } else if randomOne === 28 {
+                let applyClass = document.getElementsByClassName("container")[27];
+                applyClass.className = "selected";
+               } else if randomOne === 29 {
+                let applyClass = document.getElementsByClassName("container")[28];
+                applyClass.className = "selected";
+               } else if randomOne === 30 {
+                let applyClass = document.getElementsByClassName("container")[29];
+                applyClass.className = "selected";
+               } else if randomOne === 31 {
+                let applyClass = document.getElementsByClassName("container")[30];
+                applyClass.className = "selected";
+               } else if randomOne === 32 {
+                let applyClass = document.getElementsByClassName("container")[31];
+                applyClass.className = "selected";
+               } else if randomOne === 33 {
+                let applyClass = document.getElementsByClassName("container")[32];
+                applyClass.className = "selected";
+               } else if randomOne === 34 {
+                let applyClass = document.getElementsByClassName("container")[33];
+                applyClass.className = "selected";
+               } else if randomOne === 35 {
+                let applyClass = document.getElementsByClassName("container")[34];
+                applyClass.className = "selected";
+               } else if randomOne === 36 {
+                let applyClass = document.getElementsByClassName("container")[35];
+                applyClass.className = "selected";
+               } else if randomOne === 37 {
+                let applyClass = document.getElementsByClassName("container")[36];
+                applyClass.className = "selected";
+               } else if randomOne === 38 {
+                let applyClass = document.getElementsByClassName("container")[37];
+                applyClass.className = "selected";
+               } else if randomOne === 39 {
+                let applyClass = document.getElementsByClassName("container")[38];
+                applyClass.className = "selected";
+               } else if randomOne === 40 {
+                let applyClass = document.getElementsByClassName("container")[39];
+                applyClass.className = "selected";
+               } else if randomOne === 41 {
+                let applyClass = document.getElementsByClassName("container")[40];
+                applyClass.className = "selected";
+               } else if randomOne === 42 {
+                let applyClass = document.getElementsByClassName("container")[41];
+                applyClass.className = "selected";
+               } else if randomOne === 43 {
+                let applyClass = document.getElementsByClassName("container")[42];
+                applyClass.className = "selected";
+               } else if randomOne === 44 {
+                let applyClass = document.getElementsByClassName("container")[43];
+                applyClass.className = "selected";
+               } else if randomOne === 45 {
+                let applyClass = document.getElementsByClassName("container")[44];
+                applyClass.className = "selected";
+               } else if randomOne === 46 {
+                let applyClass = document.getElementsByClassName("container")[45];
+                applyClass.className = "selected";
+               } else if randomOne === 47 {
+                let applyClass = document.getElementsByClassName("container")[46];
+                applyClass.className = "selected";
+               } else if randomOne === 48 {
+                let applyClass = document.getElementsByClassName("container")[47];
+                applyClass.className = "selected";
+               } else if randomOne === 49 {
+                let applyClass = document.getElementsByClassName("container")[48];
+                applyClass.className = "selected";
+               } else if randomOne === 50 {
+                let applyClass = document.getElementsByClassName("container")[49];
+                applyClass.className = "selected";
+               } else if randomOne === 51 {
+                let applyClass = document.getElementsByClassName("container")[50];
+                applyClass.className = "selected";
+               } else if randomOne === 52 {
+                let applyClass = document.getElementsByClassName("container")[51];
+                applyClass.className = "selected";
+               } else if randomOne === 53 {
+                let applyClass = document.getElementsByClassName("container")[52];
+                applyClass.className = "selected";
+               } else if randomOne === 54 {
+                let applyClass = document.getElementsByClassName("container")[53];
+                applyClass.className = "selected";
+               } else if randomOne === 55 {
+                let applyClass = document.getElementsByClassName("container")[54];
+                applyClass.className = "selected";
+               } else if randomOne === 56 {
+                let applyClass = document.getElementsByClassName("container")[55];
+                applyClass.className = "selected";
+               } else if randomOne === 57 {
+                let applyClass = document.getElementsByClassName("container")[56];
+                applyClass.className = "selected";
+               } else if randomOne === 58 {
+                let applyClass = document.getElementsByClassName("container")[57];
+                applyClass.className = "selected";
+               } else if randomOne === 59 {
+                let applyClass = document.getElementsByClassName("container")[58];
+                applyClass.className = "selected";
+               } else if randomOne === 60 {
+                let applyClass = document.getElementsByClassName("container")[59];
+                applyClass.className = "selected";
+               } else if randomOne === 61 {
+                let applyClass = document.getElementsByClassName("container")[60];
+                applyClass.className = "selected";
+               } else if randomOne === 62 {
+                let applyClass = document.getElementsByClassName("container")[61];
+                applyClass.className = "selected";
+               } else if randomOne === 63 {
+                let applyClass = document.getElementsByClassName("container")[62];
+                applyClass.className = "selected";
+               } else if randomOne === 64 {
+                let applyClass = document.getElementsByClassName("container")[63];
+                applyClass.className = "selected";
+               } else if randomOne === 65 {
+                let applyClass = document.getElementsByClassName("container")[64];
+                applyClass.className = "selected";
+               } else if randomOne === 66 {
+                let applyClass = document.getElementsByClassName("container")[65];
+                applyClass.className = "selected";
+               } else if randomOne === 67 {
+                let applyClass = document.getElementsByClassName("container")[66];
+                applyClass.className = "selected";
+               } else if randomOne === 68 {
+                let applyClass = document.getElementsByClassName("container")[67];
+                applyClass.className = "selected";
+               } else if randomOne === 69 {
+                let applyClass = document.getElementsByClassName("container")[68];
+                applyClass.className = "selected";
+               } else if randomOne === 70 {
+                let applyClass = document.getElementsByClassName("container")[69];
+                applyClass.className = "selected";
+               } else if randomOne === 71 {
+                let applyClass = document.getElementsByClassName("container")[70];
+                applyClass.className = "selected";
+               } else if randomOne === 72 {
+                let applyClass = document.getElementsByClassName("container")[71];
+                applyClass.className = "selected";
+               } else if randomOne === 73 {
+                let applyClass = document.getElementsByClassName("container")[72];
+                applyClass.className = "selected";
+               } else if randomOne === 74 {
+                let applyClass = document.getElementsByClassName("container")[73];
+                applyClass.className = "selected";
+               } else if randomOne === 75 {
+                let applyClass = document.getElementsByClassName("container")[74];
+                applyClass.className = "selected";
+               } else if randomOne === 76 {
+                let applyClass = document.getElementsByClassName("container")[75];
+                applyClass.className = "selected";
                }
             }
         }
